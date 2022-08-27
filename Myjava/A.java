81cfc5f65761c77d4df038f050957f3236396c88
Yatidiyora <yatidiyora13@gmail.com>
2022-08-27 17:05:17 +0530

    User defined pakage creation and import it in other pakages

---
+++ Myjava/A.java
@@ -0,0 +1,10 @@
+package Yati;
+
+public class A
+{
+	public static int x=10;
+	public void show()
+	{
+		System.out.println("Hi How Are U");
+	}
+}
---
+++ Myjava/C.java
@@ -0,0 +1,11 @@
+package Diyora;
+import Yati.A;
+class C extends A
+{
+	public static void main(String[] args){
+		A r=new A();
+		r.show();
+		System.out.println(x);
+	}
+}
+
